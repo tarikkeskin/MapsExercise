f16b853c7ba9939b698093ceab90f2eda12ac377
tarikkeskin <tarik.keskin007@gmail.com>
2022-05-10 23:39:09 +0300

    Initial commit

---
+++ app/src/main/java/com/trigoo/mapsexercise/MapsActivity.java
@@ -0,0 +1,54 @@
+package com.trigoo.mapsexercise;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.trigoo.mapsexercise.databinding.ActivityMapsBinding;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private ActivityMapsBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        binding.buttonKonumaGit.setOnClickListener(view -> {
+            //39.9111594,33.2184621,17.54z
+            LatLng konum = new LatLng(39.9111594, 33.2184621);
+            mMap.addMarker(new MarkerOptions().position(konum).title("Elmadağ")
+                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.resim)));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(konum,17f));
+            mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+
+        });
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+}
